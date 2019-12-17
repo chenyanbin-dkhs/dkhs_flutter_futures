f6aa091bf2d9e5e9857ae4af8121dbda83235dcb
chenyanbin <chenyanbin@dkhs.com>
2019-12-17 14:21:17 +0800

    头部滚动修改透明度优化

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './widgets/main_banner.dart';
+import './widgets/home_app_bar.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
 
@@ -11,33 +12,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  AnimationController _ColorAnimationController;
-  Animation<double> _colorTween;
-
+  double alpha;
   @override
   void initState() {
     super.initState();
-
-    _ColorAnimationController =
-        new AnimationController(vsync: this, duration: Duration(seconds: 0));
-    _colorTween =
-        new Tween(begin: 0.0, end: 1.0).animate(_ColorAnimationController);
   }
 
   bool _scrollListener(ScrollNotification scrollInfo) {
     if (scrollInfo is ScrollUpdateNotification && scrollInfo.depth == 0) {
-      double alpha = scrollInfo.metrics.pixels / APPBAR_SCROLL_OFFSET;
-      if (alpha >= 0 && alpha <= 1) {
-        _ColorAnimationController.animateTo(alpha);
+      double _alpha = scrollInfo.metrics.pixels / APPBAR_SCROLL_OFFSET;
+      if (_alpha < 0) {
+        _alpha = 0;
+      } else if (_alpha > 1) {
+        _alpha = 1;
+      }
+
+      if (_alpha != this.alpha) {
+        setState(() {
+          alpha = _alpha;
+        });
       }
     }
     return true;
-  }
-
-  @override
-  void dispose() {
-    _ColorAnimationController.dispose();
-    super.dispose();
   }
 
   @override
@@ -69,7 +65,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                       color: Colors.deepOrange,
                       width: 250,
                       child: Center(
-                        child: Text(_colorTween.value.toString()),
+                        child: Text(this.alpha.toString()),
                       ),
                     ),
                     Container(
@@ -95,32 +91,8 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                   ],
                 ),
               ),
-              Container(
-                height: APPBAR_SCROLL_OFFSET.toDouble(),
-                child: AnimatedBuilder(
-                  animation: _ColorAnimationController,
-                  builder: (context, child) => AppBar(
-                    backgroundColor:
-                        Color(0xFFf76836).withOpacity(_colorTween.value),
-                    elevation: 0,
-                    // titleSpacing: 0.0,
-                    title: Text(_colorTween.value.toString()),
-                    actions: <Widget>[
-                      IconButton(
-                        icon: Icon(
-                          Icons.local_grocery_store,
-                        ),
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.more_vert,
-                        ),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ),
+              HomeAppBar(
+                alpha: this.alpha,
               ),
             ],
           ),
--- lib/pages/home/widgets/home_app_bar.dart
+++ lib/pages/home/widgets/home_app_bar.dart
@@ -4,39 +4,39 @@ const APPBAR_SCROLL_OFFSET = 100;
 const APPBAR_BACKGROUND_COLOR = Color(0xFFf76836);
 
 class HomeAppBar extends StatefulWidget {
-  HomeAppBar({Key key}) : super(key: key);
+  final double alpha;
+  HomeAppBar({Key key, this.alpha = 0}) : super(key: key);
 
   @override
   _HomeAppBarState createState() => _HomeAppBarState();
 }
 
 class _HomeAppBarState extends State<HomeAppBar> with TickerProviderStateMixin {
-  AnimationController _ColorAnimationController;
-  Animation<double> _colorTween;
+  // AnimationController animationController;
+  // Animation<double> _colorTween;
 
   @override
   void initState() {
     super.initState();
 
-    _ColorAnimationController =
-        new AnimationController(vsync: this, duration: Duration(seconds: 0));
-    _colorTween =
-        new Tween(begin: 0.0, end: 1.0).animate(_ColorAnimationController);
+    // animationController =
+    //     new AnimationController(vsync: this, duration: Duration(seconds: 0));
+    // _colorTween = new Tween(begin: 0.0, end: 1.0).animate(animationController);
   }
 
-  bool _scrollListener(ScrollNotification scrollInfo) {
-    if (scrollInfo is ScrollUpdateNotification && scrollInfo.depth == 0) {
-      double alpha = scrollInfo.metrics.pixels / APPBAR_SCROLL_OFFSET;
-      if (alpha >= 0 && alpha <= 1) {
-        _ColorAnimationController.animateTo(alpha);
-      }
-    }
-    return true;
-  }
+  // bool _scrollListener(ScrollNotification scrollInfo) {
+  //   if (scrollInfo is ScrollUpdateNotification && scrollInfo.depth == 0) {
+  //     double alpha = scrollInfo.metrics.pixels / APPBAR_SCROLL_OFFSET;
+  //     if (alpha >= 0 && alpha <= 1) {
+  //       animationController.animateTo(alpha);
+  //     }
+  //   }
+  //   return true;
+  // }
 
   @override
   void dispose() {
-    _ColorAnimationController.dispose();
+    //animationController.dispose();
     super.dispose();
   }
 
@@ -44,28 +44,25 @@ class _HomeAppBarState extends State<HomeAppBar> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Container(
       height: APPBAR_SCROLL_OFFSET.toDouble(),
-      child: AnimatedBuilder(
-        animation: _ColorAnimationController,
-        builder: (context, child) => AppBar(
-          backgroundColor: Color(0xFFf76836).withOpacity(_colorTween.value),
-          elevation: 0,
-          // titleSpacing: 0.0,
-          title: Text(_colorTween.value.toString()),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.local_grocery_store,
-              ),
-              onPressed: () {},
+      child: AppBar(
+        backgroundColor: APPBAR_BACKGROUND_COLOR.withOpacity(widget.alpha ?? 0),
+        elevation: 0,
+        // titleSpacing: 0.0,
+        title: Text(widget.alpha.toString()),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.local_grocery_store,
             ),
-            IconButton(
-              icon: Icon(
-                Icons.more_vert,
-              ),
-              onPressed: () {},
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.more_vert,
             ),
-          ],
-        ),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
